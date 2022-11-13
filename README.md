b1713c62e5cf28d200fddb2396c574f4e72a1ddd
xdaTq <kujawski.erwin@gmail.com>
2022-11-13 02:17:18 +0100

    feat/added dashboard

---
+++ README.md
@@ -0,0 +1,19 @@
+# NTI Helpie
+
+## TO DO
+- [x] Change log in button visual
+- [x] Add logo to tab
+- [x] Add NTI logo
+- [x] Change font
+- [x] Board page layout
+  - [x] Message box
+  - [x] Submit forms
+  - [x] Assist buttons
+- [ ] Styling the dashboard
+  - [ ] Add button styling
+  - [ ] Fix logo (sizing / positioning)
+  - [ ] Add input styling
+- [ ] Fixing font (not showing)
+
+## NOTICE
+No safari integration 
---
+++ public/helpie_app.js
@@ -0,0 +1,31 @@
+const signInBtn = document.getElementById('signInBtn')
+const signOutBtn = document.getElementById('signOutBtn')
+
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider)
+
+signOutBtn.onclick = () => auth.signOut()
+
+///
+
+const whenSignedIn = document.getElementById('whenSignedIn')
+const whenSignedOut = document.getElementById('whenSignedOut')
+const userDetails = document.getElementById('userDetails')
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    // signed in
+    whenSignedIn.hidden = false
+    whenSignedOut.hidden = true
+    userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3> <p>User ID: ${user.uid}</p>`
+  } else {
+    // not signed in
+    whenSignedIn.hidden = true
+    whenSignedOut.hidden = false
+    userDetails.innerHTML = ''
+  }
+})
